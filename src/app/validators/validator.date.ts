475781c9f76f878ec048bd7637cab2619171b86c
joseMana <josemanangan@gmail.com>
2021-08-17 15:15:46 +0800

    - minor fix

--- src/app/validators/validator.date.ts
+++ src/app/validators/validator.date.ts
@@ -3,6 +3,22 @@ import * as moment from 'moment';
 
 export class DateValidator {
   static departureDateValidator(AC: AbstractControl) {
+    /* allow same day departure */
+    var date = new Date();
+
+    var day = date.getDate();
+    var month = (date.getMonth() + 1).toString();
+    if (Number(month) <= 10) {
+      month = "0" + month;
+    }
+    var year = date.getFullYear();
+    var dateString = year + "-" + month + "-" + day;
+    if (dateString == AC.value) {
+      return { 'dateValidator': false };
+    }
+    /* allow same day departure */
+
+
     let invalid = new Date(Date.now()).valueOf() > new Date(AC.value).valueOf();
 
     if (invalid) {
